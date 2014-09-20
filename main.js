2790437da701877273e12ff091929c536055e601
Facundo Jauregui <facundoj@Facundos-MacBook-Pro.local>
2014-09-20 16:52:09 -0300

    Fixing routes mapping

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
+/**
+ * MVC web-apps scafolding. Easy mapping for views dispatching.
+ *
+ * @author facundoj
+ *
+ * @dependency express
+ * @dependency mustache-express
+ *
+ */
 var express = require('express'),
     fs = require('fs'),
     mustacheExpress = require('mustache-express'),
     app = express(),
     routesIni = __dirname + '/routes.json';
 
+/**
+ * Maps route with action.
+ * @param route {object} configuration
+ */
+function mapRoute (route) {
+    var url = route.url || '',
+        method = String.prototype.toLowerCase.apply(route.method || ''),
+        controllerName = route.controller || '',
+        actionName = route.action || '',
+        controller;
+
+    if (!actionName || !method || !url || !controllerName) {
+        console.warn('Required information was not provided');
+        return;
+    }
+
+    try {
+        controller = require('./modules/' + controllerName + '.js');
+    } catch (err) {
+        console.warn('Controller ' + controllerName + ' doesn\'t exist');
+        return;
+    }
+
+    console.info(method.toUpperCase() + ' ' + url + ': Handled by ' + controllerName + '.' + actionName);
+    app[method](url, function(req, res) {
+        // @todo: Make view dynamic
+        res.render('index', controller.actionsBound[actionName](req, res));
+    });
+}
+
+// Reading routes.json config file and mapping
 fs.readFile(routesIni, 'utf8', function (err, routes) {
     if (err) {
-        console.log('Error: ' + err);
+        console.error('Error: ' + err);
         return;
     }
     routes = JSON.parse(routes);
-    // At this point, "routes" contains the configs
-    var i, route;
+    // At this point, "routes" is a config object
+    var i, route, url, method, controller, action;
     for (i = 0; i < routes.length; i+=1) {
         // @todo: Review. A map would be a good idea. Check express.js docs!!
-        // @todo: Why always dispatching to the very last mapped route?? need to figure it out
-        // @todo: A lot of stuff missing here. Params validation. I should check docs
-        // @todo: Also try-catching for missing required controllers
         route = routes[i];
-        console.log('Mapping ' + route.method + ': ' + route.url + ' with ' + route.controller + '.' + route.action);
-        app[String.prototype.toLowerCase.apply(route.method)](route.url, function(req, res) {
-            var controller = require('./modules/' + route.controller + '.js');
-            res.render('index', controller.actionsBound[route.action](req, res));
-        });
+        mapRoute(route);
     }
 });
 
@@ -33,5 +66,5 @@ app.set('views', __dirname + '/views');
 
 // Starting the server
 var server = app.listen(3000, function () {
-    console.log('Listening on port %d', server.address().port);
+    console.log('Server started at' + server.address().port);
 });
